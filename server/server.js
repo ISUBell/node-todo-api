a3a7270d1921ed54e48ebf4210179eaf6f47cec0
IowaState-Bell <mbell.isu@gmail.com>
2017-02-12 21:00:48 -0600

    =Finished GET:id method

--- server/server.js
+++ server/server.js
@@ -38,9 +38,18 @@ app.get('/todos/:id', (req, res) => {
 
   //validate id
   if (!ObjectID.isValid(id)) {
-    console.log('ID not valid');
     return res.status(404).send();
   }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
   // delete Todo route
