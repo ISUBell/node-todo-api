771cb87d4a00f014105cb1b5347265fbd1711dae
IowaState-Bell <mbell.isu@gmail.com>
2017-02-15 20:32:43 -0600

    =Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 //use Port if available, otherwise local port is 3000
@@ -114,8 +115,6 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
-// POST /users
-//use pick (as in patch method, email &password)
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -130,7 +129,9 @@ app.post('/users', (req, res) => {
   })
 });
 
-
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
