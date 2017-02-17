cbe9ade033e9a9fcc0620f9d237322d9c75bbe97
IowaState-Bell <mbell.isu@gmail.com>
2017-02-17 12:23:53 -0600

    =Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -68,11 +68,33 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.token': token,
     'tokens.access': 'auth'
   });
-}
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+            reject();
+        }
+
+      });
+      //use bcrypt.compare to compare password and user.password
+    });
+  });
+};
 
 UserSchema.pre('save', function (next) {
   var user = this;
-  
+
   if (user.isModified('password')) {
     bcrypt.genSalt(12, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
